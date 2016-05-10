cd59cd2c56aef82ea85f4927f16adf143acb9b61
Vitchyr Pong <vitchyr@gmail.com>
2016-05-10 18:11:29 -0400

    making progress on visualizer. Now just need to update board

--- javascript/game.js
+++ javascript/game.js
@@ -12,31 +12,100 @@
     var global = (function(){return this;})();
 
     var dataset = JSON.parse(data);
-    console.log(dataset[0]);
+    var simStep = 0;
+    var state = dataset[simStep];
+
+    var nSquaresX = 8;
+    var nSquaresY = 8;
+    var squareSize = 50;
+    var squarePadding = 1;
+    var w = nSquaresX * (squareSize + squarePadding);
+    var h = nSquaresY * (squareSize + squarePadding);
+
+    function addRow(rects, y) {
+        rects.data(state[y])
+            .enter()
+            .append("rect")
+            .attr("fill", function(d) {
+                if (d) {
+                    return "black";
+                } else {
+                    return "white";
+                }
+            })
+            .attr("x", function(d, i) {
+                return i * (squareSize + squarePadding);
+            })
+            .attr("y", y * (squareSize + squarePadding))
+            .attr("width", squareSize)
+            .attr("height", squareSize)
+            .attr("class", "square row-" + y);
+    }
+
+    function createBoard(svg, simStep) {
+        console.log("Create Board");
+        console.log(state);
+        var rects = svg.selectAll("rect")
+        for (var y = 0; y < state.length; y++) {
+            addRow(rects, y);
+        }
+    }
+
+    function updateRow(allRects, y) {
+        var rects = allRects.selectAll("square .row-" + y)
+            .data(state[y])
+        rects.attr("fill", function(d) {
+                if (d) {
+                    return "black";
+                } else {
+                    return "white";
+                }
+            })
+            .attr("x", function(d, i) {
+                return i * (squareSize + squarePadding);
+            })
+            .attr("y", y * (squareSize + squarePadding))
+            .attr("width", squareSize)
+            .attr("height", squareSize)
+            .attr("class", "square");
+    }
+    function updateBoard(svg) {
+        console.log("Update Board");
+        console.log(state);
+        var rects = svg.selectAll("rect")
+        for (var y = 0; y < state.length; y++) {
+            updateRow(rects, y);
+        }
+    }
 
     global.onload = function(){
-        var dataset = [ 5, 10, 15, 20, 25 ];
-        d3.select("body").selectAll("p")
-            .data(dataset)
-            .enter()
-            .append("p")
-            .text(function(d) { return d; });
+        var svg = d3.select(".board")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
-        var w = 500;
-        var h = 50;
+        createBoard(svg);
 
-        var svg = d3.select("body").append("svg");
-        var circles = svg.selectAll("circle")
-            .data(dataset)
-            .enter()
-            .append("circle")
-        circles.attr("cx", function(d, i) {
-                return (i*50) + 25;
-            })
-            .attr("cy", h/2)
-            .attr("r", function(d) {
-                return d;
-            })
-            .attr("fill", "red");
+        var next = document.getElementById("next-button");
+        next.onclick = function() {
+            if (simStep + 1 < dataset.length) {
+                simStep++;
+            } else {
+                console.log("Can't go farther.");
+            }
+            state = dataset[simStep];
+            updateBoard(svg);
+        };
+
+        var previous = document.getElementById("previous-button");
+        previous.onclick = function() {
+            if (simStep > 1) {
+                simStep--
+            } else {
+                console.log("Can't go farther back in simulation.");
+            }
+            state = dataset[simStep];
+            updateBoard(svg);
+        };
     };
 })();
